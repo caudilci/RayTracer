b94df0de9def0384418580f85c6bcc3d3df45b7b
Chris Caudill <caudilci@plu.edu>
2020-12-16 12:08:32 -0800

    Stubbed ColorTest

---
+++ src/test/java/utils/ColorTest.java
@@ -0,0 +1,32 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void multiplyColor() {
+    }
+
+    @Test
+    void clamp() {
+    }
+
+    @Test
+    void toInteger() {
+    }
+}
