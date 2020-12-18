a3d51f12ce6318acfac60859cebde5e8a2561788
Chris Caudill <caudilci@plu.edu>
2020-12-17 18:46:47 -0800

    Removed clamp methods utility methods, adjusted clamp to utilize Math.max and Math.min

--- src/main/java/utils/Color.java
+++ src/main/java/utils/Color.java
@@ -17,49 +17,42 @@ public class Color {
         this.r = r;
         this.g = g;
         this.b = b;
-        clamp();
     }
 
     public Color(Color color){
         r = color.r;
         g = color.g;
         b = color.b;
-        clamp();
     }
 
     public void add(Color color) {
         r += color.r;
         g += color.g;
         b += color.b;
-        clamp();
     }
 
     public void divide(int scalar){
         r /= scalar;
         g /= scalar;
         b /= scalar;
-        clamp();
     }
 
     public void multiply(double val){
         r *= val;
         g *= val;
         b *= val;
-        clamp();
     }
 
     public Color multiplyColor(Color color){
-        Color result = new Color(r*color.r, g*color.g, b*color.b);
-        result.clamp();
-        return result;
+        return new Color(r*color.r, g*color.g, b*color.b);
     }
 
     public void clamp(){
-        r = r<0 ? 0: r;
+        r = Math.max(r, 0f);
         r = Math.min(r, 0.999f);
-        g = g<0 ? 0: g;
+        g = Math.max(g, 0f);
         g = Math.min(g, 0.999f);
-        b = b<0 ? 0: b;
+        b = Math.max(b, 0f);
         b = Math.min(b, 0.999f);
     }
 
