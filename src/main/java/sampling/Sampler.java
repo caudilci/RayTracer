37c9afe39c60c8f9210834352013fd35fe6986d8
Chris Caudill <caudilci@plu.edu>
2021-01-02 20:35:03 -0800

    Implemented abstract Sampler class

---
+++ src/main/java/sampling/Sampler.java
@@ -0,0 +1,11 @@
+package sampling;
+
+import utils.Point2D;
+
+public abstract class Sampler {
+
+    private int worldWidth, woldHeight;
+
+    public abstract Point2D sample(int row, int col, int x, int y);
+
+}
